8ee899e93ddc2b4e59c35e00f635e0a33b05b750
Shawiiz_z <shaweereso@gmail.com>
2025-05-20 23:00:14 +0200

    feat: remove useless /versions from controller

--- src/main/java/com/mohistmc/controller/BuildController.java
+++ src/main/java/com/mohistmc/controller/BuildController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/project/{projectName}/versions/{versionName}")
+@RequestMapping("/project/{projectName}/{versionName}")
 @RequiredArgsConstructor
 public class BuildController {
     private final BuildService buildService;
