55e8bb3249e63c7fc65aa72a9ca215035a373481
Mgazul <mgazul@163.com>
2025-06-01 00:19:07 +0800

    feat: add all Youer versions

--- src/main/resources/db/migration/V20250512222223__populate_projects.sql
+++ src/main/resources/db/migration/V20250512222223__populate_projects.sql
@@ -4,3 +4,6 @@ VALUES ('mohist', 'mohistmc', 'mohist', 'GITHUB');
 INSERT INTO project (name, repository_owner, repository_name, git_platform)
 VALUES ('banner', 'mohistmc', 'banner', 'GITHUB');
 
+INSERT INTO project (name, repository_owner, repository_name, git_platform)
+VALUES ('youer', 'mohistmc', 'youer', 'GITHUB');
+
